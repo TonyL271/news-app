ff67495c2a35b65ab954628226ca202d5d0f22c7
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-06-22 15:19:31 -0700

    Added masonry to the layout

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,8 @@
 import Box from '@mui/material/Box';
 import ArticleCardButton from './ArticleCardButton';
-
-function App({ imgSrc, title, newsSource, desc, width, height }) {
+function App({ imgSrc, title, newsSource, desc,imgWidth, height }) {
   return (
-    <Box className="article-card" sx={{ width, height,display:'flex' ,flexDirection:'column',fontFamily: '\'PT Sans\', sans-serif'}}>
+    <Box className="article-card grid-item" sx={{ width:imgWidth, height,display:'flex' ,flexDirection:'column',fontFamily: '\'PT Sans\', sans-serif'}}>
       <Box component="img" className='article-image' src={imgSrc} sx={{ borderRadius: '6px 6px 0 0', width: '100%' }}></Box>
       <Box className="details" sx={{backgroundColor:'#FCFEFE', px:'2rem'}}>
         <Box component="p" sx={{fontWeight:'bold',fontSize:'1rem',}} className="article-title">{title}</Box>
@@ -12,9 +11,9 @@ function App({ imgSrc, title, newsSource, desc, width, height }) {
       </Box >
       <Box className="card-buttons" sx={{ width: '100%', display: 'flex' }}>
         <ArticleCardButton content="like" sx={{ borderRadius: '0 0 0 6px'}}></ArticleCardButton>
-        <Box className="seperator" sx={{height:'100%',width:'2px', backgroundColor:'#E6F5FB'}}></Box>
+        <Box className="seperator" sx={{height:'100%',width:'5px', backgroundColor:'#E6F5FB'}}></Box>
         <ArticleCardButton content="Comment"></ArticleCardButton>
-        <Box className="seperator" sx={{height:'100%',width:'2px', backgroundColor:'#E6F5FB'}}></Box>
+        <Box className="seperator" sx={{height:'100%',width:'5px', backgroundColor:'#E6F5FB'}}></Box>
         <ArticleCardButton content="Share" sx={{ borderRadius: '0 0 6px 0' }}></ArticleCardButton>
       </Box>
     </Box>
--- src/components/ArticleGrid.jsx
+++ src/components/ArticleGrid.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import { ArticleCard } from '.'
 import Box from '@mui/material/Box';
+import Masonry from 'react-masonry-css'
 
 const imgSrc = 'http://ipravda.sk/res/2020/05/26/thumbs/atom-clanokW.jpg'
+const title = "Why can't you trust atoms?"
+const newsSource = 'Fake News Broadcasting company'
+const desc = "Because they make everything up. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+const imgWidths = [80,60,40,50];
+
 
 const ArticleGrid = () => {
-  return (
-    <Box className='article-grid'>
-      <ArticleCard
-        width='33.33%'
-        imgSrc={imgSrc}
-        title="Why can't you trust atoms?"
-        newsSource={'Fake News Broadcasting company'}
-        desc="Because they make everything up. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
-      />
-      <ArticleCard
-        imgSrc={imgSrc}
-        width='16.66%'
-        title="Why can't you trust atoms?"
-        newsSource={'Fake News Broadcasting company'}
-        desc="Because they make everything up. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
-      />
-    </Box>
 
+  return (
+    <Masonry
+      breakpointCols={3}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column">
+      {imgWidths.map((imgWidth, idx) => (<ArticleCard key={idx} imgWidth={imgWidth + '%'} imgSrc={imgSrc} title={title} newsSource={newsSource} desc={desc} />))}
+    </Masonry>
   )
 }
 
