43ee5097c98b7054905c9a85f2bc4d6995210c1c
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-06-25 10:39:28 -0700

    Downloaded news api response so that a the api doesn't need to be called on each render for testing purposes.

--- src/components/ArticleGrid.jsx
+++ src/components/ArticleGrid.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { ArticleCard } from '.'
-// import Masonry from 'react-masonry-css'
 import Masonry from 'react-masonry-component';
+import newsData from '../data/newsData.json'
 const masonryOptions = {
   transitionDuration: 0
 };
 const imagesLoadedOptions = { background: '.article-card' }
 
 const getWidth = () => {
-  console.log('object');
   let width = Math.floor(50 / Math.pow(2, Math.random() * 3))
-  return `${Math.max(width,20)}%`;
-
+  return `${Math.max(width, 20)}%`;
 }
 
 
@@ -21,28 +19,37 @@ const ArticleGrid = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [articles, setArticles] = useState([]);
   const [urls, setUrls] = useState({});
-  const url = 'https://newsapi.org/v2/everything?' +
-    'q=Apple&' +
-    'from=2022-06-15&' +
-    'sortBy=popularity&' +
-    'apiKey=407bfef306444abbba571029556f5c5a';
+  // Uncomment when in production
+  // const url = 'https://newsapi.org/v2/everything?' +
+  //   'q=Apple&' +
+  //   'from=2022-06-15&' +
+  //   'sortBy=popularity&' +
+  //   'apiKey=407bfef306444abbba571029556f5c5a';
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setArticles(result.articles);
+  //         setUrls(result.articles.urlToImage);
+  //       },
+  //       // Note: it's important to handle errors here
+  //       // instead of a catch() block so that we don't swallow
+  //       // exceptions from actual bugs in components.
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     )
+  // }, [])
+
   useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setArticles(result.articles);
-          setUrls(result.articles.urlToImage);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
+
+    setArticles(newsData.articles);
+    setUrls(newsData.articles.urlToImage);
+
+
   }, [])
 
   return (
@@ -56,7 +63,7 @@ const ArticleGrid = () => {
     >
       {
         // articles.map((article) => <img style={{width:'100%'}} src={article.urlToImage}></img>)
-        articles.map((article, idx) => <ArticleCard key={idx} title={article.title} newsSource={article.source.name} desc={article.description} style={{ width: getWidth(),margin:'3rem' }} imgSrc={article.urlToImage}></ArticleCard>)
+        articles.map((article, idx) => <ArticleCard key={idx} title={article.title} newsSource={article.source.name} desc={article.description} style={{ width: getWidth(), margin: '3rem' }} imgSrc={article.urlToImage}></ArticleCard>)
         // imgWidths.map((imgWidth, idx) => (<ArticleCard key={idx} imgwidth={imgwidth + '%'} imgheight={imgwidth + '%'} imgsrc={imgsrc} title={title} newssource={newssource} desc={desc} />))
       }
     </Masonry >
